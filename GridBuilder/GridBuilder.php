60b67aaf9b257ef3b669fe5fe35fe3727bc090fd
Mariusz <Mariusz@Mariusz>
2015-08-24 16:00:40 +0200

    Merge origin/master
    Conflicts:
    	DependencyInjection/Configuration.php
    	Resources/config/routing.yml
    	Resources/config/services.yml
    	Tests/Controller/DefaultControllerTest.php
    	composer.json

--- GridBuilder/GridBuilder.php
+++ GridBuilder/GridBuilder.php
@@ -4,15 +4,16 @@ namespace TMSolution\DataGridBundle\GridBuilder;
 
 class GridBuilder
 {
-
+    protected $container;
+    
+    public function __construct($container)
+    {
+        $this->container=$container;
+    }
     
     public function buildGrid($grid,$routePrefix)
     {
         return $grid;
     }
-
-
-
-  
 
 }
